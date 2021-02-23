379da9a2f633d2e87180a853b776ef9104fd038a
carlotalobo <carlota.lobo.sanz@gmail.com>
2021-02-23 16:43:53 +0100

    Add project files.

---
+++ ServiceNumeroDoble/INumeroDobleContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceNumeroDoble
+{
+    [ServiceContract] //indicamos que forma parte del servicio, la anotacion viene de syste service model
+    public interface INumeroDobleContract
+    {
+        [OperationContract] // los metodos que quiera para el servicio se anotan
+        int GetNumeroDoble(int numero);
+        [OperationContract]
+        String GetSaludo(String nombre);
+        String Invisible();
+    }
+}
---
+++ ServiceNumeroDoble/NumeroDobleImpl.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceNumeroDoble
+{
+    public class NumeroDobleImpl : INumeroDobleContract
+    {
+        public int GetNumeroDoble(int numero)
+        {
+            return numero * 2;
+        }
+
+        public string Invisible()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSaludo(string nombre)
+        {
+            return "Hola, " +  nombre;
+        }
+    }
+}
